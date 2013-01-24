257de3055b0bca7cb11493b4d6ea17ebb189d84d
Yongzhi Pan <panyongzhi@meituan.com>
2013-01-24 14:55:42 +0800

    Use cgitb HTML output.
    This produces more useful and friendlier exception info.

--- exception_notifier.py
+++ exception_notifier.py
@@ -17,9 +17,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import smtplib
 import socket
 import traceback
+import cgitb
+import sys
 
 
 def send_email(sender, receivers, subject, body, mail_server):
@@ -33,15 +36,24 @@ def send_email(sender, receivers, subject, body, mail_server):
 
     This function should be robust.
     '''
-    msg = MIMEText(body)
+    msg = MIMEMultipart('alternative')
     msg['subject'] = str(subject).decode('utf-8')
     msg['From'] = sender
     msg['To'] = ','.join(receivers)
+    msg.attach(MIMEText(body, 'html', 'utf-8'))
 
     s = smtplib.SMTP(mail_server)
     s.sendmail(msg['From'], receivers, msg.as_string())
     print "'%s' sent to %s" % (subject, ','.join(receivers))
     s.quit()
+
+
+class WriteableObject(file):
+    def __init__(self):
+        self.content = ''
+
+    def write(self, string):
+        self.content += string
 
 
 def mail_exception(sender, receivers, mail_server='localhost'):
@@ -58,8 +70,16 @@ def mail_exception(sender, receivers, mail_server='localhost'):
             except Exception as e:
                 hostname = socket.gethostname()
                 subject = '%s:%s: %s' % (hostname, __file__, e)
-                body = traceback.format_exc()
-                print body
+                traceback.print_exc()
+
+                # Write cgitb output to a variable
+                bodyf = WriteableObject()
+                sys_stderr_orig = sys.stderr
+                sys.stderr = bodyf
+                cgitb.Hook(file=bodyf, context=7, format='html').handle()
+                sys.stderr = sys_stderr_orig
+                body = bodyf.content
+
                 send_email(sender, receivers, subject, body, mail_server)
         return wrapper
     return decorator
