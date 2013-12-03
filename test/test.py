7e1d762e86aae1ee4ec566d5e59e28e57c513ac4
Yongzhi Pan <panyongzhi@gmail.com>
2013-12-03 19:11:42 +0800

    Add more tests with unittest.

--- test/test.py
+++ test/test.py
@@ -1,52 +1,84 @@
 #! /usr/bin/env python
 
-"""Test script.
+"""Test script for Travis CI.
 
-
-Test mail function and custom callback function.
->>> f(1,0)
------
-type: <type 'exceptions.ZeroDivisionError'>
-value: integer division or modulo by zero
------
+Email sending cannot be tested since it is impossible to send email on
+Travis CI.
 """
 
 import sys
+import unittest
 
 # Must run from the project root.
 sys.path.insert(0, '.')
-import exception_notifier
-
-
-def exc_handler():
-    typ, value = sys.exc_info()[:2]
-    print '-----'
-    print 'type:', typ
-    print 'value:', value
-    print '-----'
+import exception_notifier as en
 
 
 def exc_handler_ret():
     """An exception handler that returns something other than None."""
     return 'DIV_BY_ZERO'
 
-# Email sending is not tested since it is impossible to send email on Travis
-# server.
-@exception_notifier.mail_exception(callback=exc_handler)
-def f(x, y):
+
+def div(x, y):
     return x / y
 
 
-@exception_notifier.mail_exception(callback=exc_handler_ret)
-def g(x, y):
-    return x / y
+class TestMailException(unittest.TestCase):
+    """ Test mail exception function.
+    """
+    def test_mailexc(self):
+        self.assertRaises(Exception, en.mail_exception()(div)(2, 0))
+
+    def test_mailexc_withcb(self):
+        f = en.mail_exception(callback=exc_handler_ret, both=True)(div)
+        self.assertRaises(Exception, f(2, 0))
+        self.assertEqual(f(2, 0), 'DIV_BY_ZERO')
+
+    def test_mailexc_onlycb(self):
+        g = en.mail_exception(callback=exc_handler_ret)(div)
+        self.assertRaises(Exception, g(2, 0))
+        self.assertEqual(g(2, 0), 'DIV_BY_ZERO')
+
+
+class TestExceptionHook(unittest.TestCase):
+    """ Test case for exception hook.
+    """
+    def setUp(self):
+        en.enable()
+
+    def test_hook(self):
+        self.assertRaises(ZeroDivisionError, div, 3, 0)
+
+    def tearDown(self):
+        en.disable()
+
+
+class TestExceptionHookWithCB(unittest.TestCase):
+    """ Test case for exception hook.
+    """
+    def setUp(self):
+        en.enable(callback=exc_handler_ret)
+
+    def test_hook(self):
+        self.assertRaises(ZeroDivisionError, div(3, 0))
+        self.assertEqual(div(3, 0), 'DIV_BY_ZERO')
+
+    def tearDown(self):
+        en.disable()
+
+
+class TestExceptionHookOnlyCB(unittest.TestCase):
+    """ Test case for exception hook.
+    """
+    def setUp(self):
+        en.enable()
+
+    def test_hook(self):
+        self.assertRaises(ZeroDivisionError, div, 3, 0)
+
+    def tearDown(self):
+        en.disable()
 
 
 if __name__ == '__main__':
-    # Test return value of function call.
-    assert f(2, 1) == 2
-    # Test return value of the custom callback function.
-    assert g(1, 0) == 'DIV_BY_ZERO'
-
-    import doctest
-    doctest.testmod(raise_on_error=True)
+    unittest.main(verbosity=2)
